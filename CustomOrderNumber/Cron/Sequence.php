6f521ca1b996e2043d58253c29e5a9962fd6cd1d
NhatSon <sonln.fbn@gmail.com>
2017-08-25 14:03:26 +0700

    done 1

--- CustomOrderNumber/Cron/Sequence.php
+++ CustomOrderNumber/Cron/Sequence.php
@@ -1,4 +1,32 @@
 <?php
+/**
+ * BSS Commerce Co.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://bsscommerce.com/Bss-Commerce-License.txt
+ *
+ * =================================================================
+ *
+ * MAGENTO EDITION USAGE NOTICE
+ * =================================================================
+ * This package designed for Magento COMMUNITY edition
+ * BSS Commerce does not guarantee correct work of this extension
+ * on any other Magento edition except Magento COMMUNITY edition.
+ * BSS Commerce does not provide extension support in case of
+ * incorrect edition usage.
+ * =================================================================
+ *
+ * @category   BSS
+ * @package    Bss_CustomOrderNumber
+ * @author     Extension Team
+ * @copyright  Copyright (c) 2015-2016 BSS Commerce Co. ( http://bsscommerce.com )
+ * @license    http://bsscommerce.com/Bss-Commerce-License.txt
+ */
+
 namespace Bss\CustomOrderNumber\Cron;
 
 class Sequence 
@@ -12,20 +40,6 @@ class Sequence
     	$this->storeManager = $storeManager;
     	$this->sequence = $sequence;
     }	
-
- 	public function cronYYYY() 
-    {
-    	$frequency = 0;
-    	$storeId = 0;
-    	$this->sequence->setCron($storeId, $frequency);
-    	$stores = $this->storeManager->getStores();
-    	foreach($stores as $store) {
-    		$storeId = $store->getStoreId();
-    		$this->sequence->setCron($storeId, $frequency);
-    	}
-    	return $this;
-    }
-
     public function cronDaily() 
     {
     	$frequency = 1;
--- CustomOrderNumber/Model/Config/Source/Frequency.php
+++ CustomOrderNumber/Model/Config/Source/Frequency.php
@@ -28,7 +28,7 @@
  */
 namespace Bss\CustomOrderNumber\Model\Config\Source;
 
-class ResetSelect implements \Magento\Framework\Option\ArrayInterface
+class Frequency implements \Magento\Framework\Option\ArrayInterface
 {
     /**
      * @const Position
